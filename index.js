be7c048c225fe85437890c74200880c9e31e5fdc
BakiAbdullah <shatil605@gmail.com>
2023-07-24 19:04:04 +0600

    dynamic Api for single college and searching college api added

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ app.use(cors(corsOptions));
 app.use(express.json());
 app.use(morgan("dev"));
 
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ap9yj43.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -85,6 +85,26 @@ async function run() {
         .toArray();
       res.send(result);
     });
+
+    // Get a Single College Data
+    app.get("/allcolleges/:id", async (req, res) => {
+      const id = req.params.id;
+      const result = await collegeCollection.findOne({ _id: new ObjectId(id) });
+      res.send(result);
+    });
+
+     //TODO: Search College by College name
+    // app.get("/searchCollege/:text", async (req, res) => {
+    //   const queryText = req.params.text;
+    //   if (!queryText) {
+    //     return res.status(400).json({ error: "Query text is required" });
+    //   }
+    //   const result = await collegeCollection
+    //     .find({ collegeName: { $regex: queryText, $options: "i" } })
+    //     .toArray();
+    //   res.send(result);
+    // });
+
 
     // << Class Related Routes Ends >>>
 
